ee074121fd316729de463f1da31c98c54372090b
GitHub Action <action@github.com>
2021-11-28 17:22:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/berezhkoe/kindmotivator/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/berezhkoe/kindmotivator/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.berezhkoe.kindmotivator.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.berezhkoe.kindmotivator.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/berezhkoe/kindmotivator/services/MyProjectService.kt
+++ src/main/kotlin/com/github/berezhkoe/kindmotivator/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.berezhkoe.kindmotivator.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.berezhkoe.kindmotivator.MyBundle
 
 class MyProjectService(project: Project) {
 
