aeb5b20767b1f9cf56b515bd2cf419ba6a5ab28c
vladkash <spanch-kash@yandex.ru>
2021-11-30 16:53:26 +0300

    fix using new show meme api

--- src/main/kotlin/com/github/berezhkoe/kindmotivator/listeners/MotivationUtils.kt
+++ src/main/kotlin/com/github/berezhkoe/kindmotivator/listeners/MotivationUtils.kt
@@ -5,20 +5,14 @@ import com.github.berezhkoe.kindmotivator.notification.MotivationType
 import com.intellij.openapi.application.invokeLater
 import com.intellij.openapi.project.Project
 
-internal fun motivateOpen(project: Project) {
-    invokeLater {
-        MotivationShower.showMeme(MotivationType.Praise, "Штош, за работу!", "original.gif", project)
-    }
-}
-
 internal fun motivateFail(project: Project) {
     invokeLater {
-        MotivationShower.showMeme(MotivationType.Praise, "Крепись...", "original.gif", project)
+        MotivationShower.showMeme(MotivationType.Support, "Крепись...", "klichko.jpg", project)
     }
 }
 
 internal fun motivateSuccess(project: Project) {
     invokeLater {
-        MotivationShower.showMeme(MotivationType.Praise, "Ура!", "original.gif", project)
+        MotivationShower.showMeme(MotivationType.Praise, "Ура!", "leonardo-dicaprio-cheers.gif", project)
     }
 }
