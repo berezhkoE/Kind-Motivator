903312760e7cc9fa9a47a6df5b9ccb98dd07b85d
vladkash <spanch-kash@yandex.ru>
2021-11-30 14:28:19 +0300

    proof on concept

--- src/main/kotlin/com/github/berezhkoe/kindmotivator/listeners/CounterUtils.kt
+++ src/main/kotlin/com/github/berezhkoe/kindmotivator/listeners/CounterUtils.kt
@@ -1,14 +1,20 @@
 package com.github.berezhkoe.kindmotivator.listeners
 
+import com.github.berezhkoe.kindmotivator.notification.MemeNotificationService
 import com.intellij.ide.util.PropertiesComponent
+import com.intellij.openapi.application.invokeLater
 import com.intellij.openapi.project.Project
 
 internal fun motivateFail(project: Project) {
-    TODO("motivate fail!!!!")
+    invokeLater {
+        MemeNotificationService.getInstance().showMeme("Молодец!", MemeNotificationService.memePath("praise", "original.gif"), project)
+    }
 }
 
 internal fun motivateSuccess(project: Project) {
-    TODO("motivate success!!!!")
+    invokeLater {
+        MemeNotificationService.getInstance().showMeme("Молодец!", MemeNotificationService.memePath("praise", "original.gif"), project)
+    }
 }
 
 internal fun getCounter(name: String) : Int {
