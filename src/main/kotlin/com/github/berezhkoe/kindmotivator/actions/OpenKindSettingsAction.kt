47ccfcc25c0b3556bfcd5d5c791aa255a45f80b5
Boris Pristupa <boris.pristupa@jetbrains.com>
2021-12-01 20:07:36 +0300

    make settings showing action implement DumbAware

--- src/main/kotlin/com/github/berezhkoe/kindmotivator/actions/OpenKindSettingsAction.kt
+++ src/main/kotlin/com/github/berezhkoe/kindmotivator/actions/OpenKindSettingsAction.kt
@@ -1,11 +1,11 @@
 package com.github.berezhkoe.kindmotivator.actions
 
 import com.github.berezhkoe.kindmotivator.settings.KindConfigurable
-import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.options.ShowSettingsUtil
+import com.intellij.openapi.project.DumbAwareAction
 
-class OpenKindSettingsAction: AnAction() {
+class OpenKindSettingsAction: DumbAwareAction() {
     override fun actionPerformed(e: AnActionEvent) {
         ShowSettingsUtil.getInstance().showSettingsDialog(e.project!!, KindConfigurable::class.java)
     }
